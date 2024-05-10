e6095a2b2add106a5de7623d7e56a46c304905df
Thiago Silva <webthiagosilva@gmail.com>
2024-05-09 23:04:31 -0300

    feat: implement word to number converter rules and tests

---
+++ src/Exercises/Exercise3/WordToNumberConverter.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exercises\Exercise3;
+
+class WordToNumberConverter
+{
+	private const OFFSET_LOWERCASE = 'a';
+	private const OFFSET_UPPERCASE = 'A';
+	private const BASE_VALUE_LOWERCASE = 1;
+	private const BASE_VALUE_UPPERCASE = 27;
+
+	public function convertToNumber(string $word): int
+	{
+		$sum = 0;
+		foreach (str_split($word) as $char) {
+			$sum += $this->getCharValue($char);
+		}
+
+		return $sum;
+	}
+
+	private function getCharValue(string $char): int
+	{
+		if (ctype_alpha($char)) {
+			return $this->calculateValue($char);
+		}
+
+		return 0;
+	}
+
+	private function calculateValue(string $char): int
+	{
+		$offset = ctype_lower($char) ? ord(self::OFFSET_LOWERCASE) : ord(self::OFFSET_UPPERCASE);
+		$baseValue = ctype_lower($char) ? self::BASE_VALUE_LOWERCASE : self::BASE_VALUE_UPPERCASE;
+
+		return ord($char) - $offset + $baseValue;
+	}
+}
---
+++ tests/Feature/WordToNumberConverterTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+use App\Exercises\Exercise3\WordToNumberConverter;
+
+class WordToNumberConverterTest extends TestCase
+{
+	public function wordProvider()
+	{
+		return [
+			['abc', 1 + 2 + 3],
+			['ABC', 27 + 28 + 29],
+			['aBc', 1 + 28 + 3],
+			['zZ', 26 + 52],
+			['123', 0],
+			['!@#$', 0],
+			['aBc123', 1 + 28 + 3],
+			['Jesus is the king', 202]
+		];
+	}
+
+	/**
+	 * @dataProvider wordProvider
+	 */
+	public function testConvertToNumber(string $word, int $expectedSum)
+	{
+		$converter = new WordToNumberConverter();
+		$this->assertEquals($expectedSum, $converter->convertToNumber($word));
+	}
+}
