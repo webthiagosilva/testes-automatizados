554cfae484ddf85fb800e2485de17b501ea23e3d
Thiago Silva <webthiagosilva@gmail.com>
2024-05-09 22:11:47 -0300

    feat: implement happy number cheker your tests

--- src/Exercises/Exercise2/HappyNumberCalculator.php
+++ src/Exercises/Exercise2/HappyNumberCalculator.php
@@ -4,35 +4,35 @@ declare(strict_types=1);
 
 namespace App\Exercises\Exercise2;
 
-use InvalidArgumentException;
+use App\Common\NumberUtil;
 
 class HappyNumberCalculator
 {
 	private const HAPPY_NUMBER = 1;
 
-	private SquareDigitSumCalculator $calculator;
-
-	public function __construct(SquareDigitSumCalculator $calculator)
-	{
-		$this->calculator = $calculator;
-	}
-
 	public function isHappy(int $number): bool
 	{
-		$this->validateNumber($number);
+		if ($number < self::HAPPY_NUMBER) return false;
 
 		$seen = [];
 		while (!$this->hasReachedHappyOrRepeatedState($number, $seen)) {
 			$seen[$number] = true;
-			$number = $this->calculator->sumOfSquaresOfDigits($number);
+			$number = $this->sumOfSquaresOfDigits($number);
 		}
 
 		return $number === self::HAPPY_NUMBER;
 	}
 
-	private function validateNumber(int $number): void
+	private function sumOfSquaresOfDigits(int $number): int
 	{
-		if ($number < 1) throw new InvalidArgumentException('Number must be greater than 0.');
+		$sum = 0;
+		while ($number > 0) {
+			$digit = NumberUtil::extractLastDigit($number);
+			$sum += NumberUtil::calculateSquare($digit);
+			$number = NumberUtil::removeLastDigit($number);
+		}
+
+		return $sum;
 	}
 
 	private function hasReachedHappyOrRepeatedState(int $number, array $seen): bool
---
+++ tests/Feature/HappyNumberCalculatorTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+use App\Exercises\Exercise2\HappyNumberCalculator;
+
+class HappyNumberCalculatorTest extends TestCase
+{
+	public function numberProvider()
+	{
+		return [
+			[7, true],
+			[97, true],
+			[10, true],
+			[2, false],
+			[3, false],
+			[4, false],
+			[145, false],
+			[89, false],
+			[20, false],
+			[116, false],
+			[1, true],
+			[0, false],
+			[-1, false],
+		];
+	}
+
+	/**
+	 * @dataProvider numberProvider
+	 */
+	public function testIsHappy(int $number, bool $expected)
+	{
+		$calculator = new HappyNumberCalculator();
+		$this->assertEquals($expected, $calculator->isHappy($number));
+	}
+}
