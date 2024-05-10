76c20f3447dde3876d5b1ca63d1f2b9efcbfdba3
Thiago Silva <webthiagosilva@gmail.com>
2024-05-09 21:28:41 -0300

    feat: implement a helper for numerical calc

---
+++ src/Common/NumberUtil.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common;
+
+class NumberUtil
+{
+	private const DECIMAL_BASE = 10;
+
+	public static function isMultiple(int $number, int $divisor): bool
+	{
+		return $number % $divisor === 0;
+	}
+
+	public static function extractLastDigit(int $number): int
+	{
+		return $number % self::DECIMAL_BASE;
+	}
+
+	public static function calculateSquare(int $digit): int
+	{
+		return $digit * $digit;
+	}
+
+	public static function removeLastDigit(int $number): int
+	{
+		return intdiv($number, self::DECIMAL_BASE);
+	}
+
+	public static function isPrime(int $number): bool
+	{
+		if ($number < 2) return false;
+
+		for ($divisor = 2; $divisor <= sqrt($number); $divisor++) {
+			if (self::isMultiple($number, $divisor)) return false;
+		}
+
+		return true;
+	}
+}
---
+++ tests/Unit/NumberUtilTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Common\NumberUtil;
+use PHPUnit\Framework\TestCase;
+
+class NumberUtilTest extends TestCase
+{
+	public function multipleProvider(): array
+	{
+		return [[10, 2, true], [15, 3, true], [14, 3, false], [100, 25, true], [7, 2, false]];
+	}
+
+	/**
+	 * @dataProvider multipleProvider
+	 */
+	public function testIsMultiple(int $number, int $divisor, bool $expected)
+	{
+		$this->assertEquals($expected, NumberUtil::isMultiple($number, $divisor));
+	}
+
+	public function lastDigitProvider(): array
+	{
+		return [[123, 3], [50, 0], [2987, 7], [1, 1]];
+	}
+
+	/**
+	 * @dataProvider lastDigitProvider
+	 */
+	public function testExtractLastDigit(int $number, int $expected)
+	{
+		$this->assertEquals($expected, NumberUtil::extractLastDigit($number));
+	}
+
+	public function squareProvider()
+	{
+		return [[3, 9], [5, 25], [10, 100], [0, 0]];
+	}
+
+	/**
+	 * @dataProvider squareProvider
+	 */
+	public function testCalculateSquare(int $digit, int $expected)
+	{
+		$this->assertEquals($expected, NumberUtil::calculateSquare($digit));
+	}
+
+	public function removeLastDigitProvider()
+	{
+		return [[1234, 123], [50, 5], [7, 0], [98765, 9876]];
+	}
+
+	/**
+	 * @dataProvider removeLastDigitProvider
+	 */
+	public function testRemoveLastDigit(int $number, int $expected)
+	{
+		$this->assertEquals($expected, NumberUtil::removeLastDigit($number));
+	}
+
+	public function primeProvider()
+	{
+		return [[2, true], [3, true], [4, false], [13, true], [25, false], [97, true], [1, false], [0, false], [-7, false]];
+	}
+
+	/**
+	 * @dataProvider primeProvider
+	 */
+	public function testIsPrime(int $number, bool $expected)
+	{
+		$this->assertEquals($expected, NumberUtil::isPrime($number));
+	}
+}
